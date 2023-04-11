64c9e44fa25700a23881aa02944fec9e3b12e14b
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-04-11 21:36:34 +0630

    Update ctor

--- lib/rental.rb
+++ lib/rental.rb
@@ -1,8 +1,12 @@
 class Rental
   def initialize(date, book, person)
     @date = date
+
     @book = book
+    @book.rentals << self
+
     @person = person
+    @person.rentals << self
   end
 
   attr_accessor :date
