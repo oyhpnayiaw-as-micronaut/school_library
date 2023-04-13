6abcd22b7e134e42531beb5e746a3b5961701ea5
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-04-13 18:58:44 +0630

    Remove comments

--- lib/app.rb
+++ lib/app.rb
@@ -1,11 +1,3 @@
-# Create a app.rb file that will serve as your console app entry-point. It should have methods that do the following:
-# List all books.
-# List all people.
-# Create a person (teacher or student, not a plain Person).
-# Create a book.
-# Create a rental.
-# List all rentals for a given person id.
-
 require_relative 'book'
 require_relative 'person'
 require_relative 'student'
